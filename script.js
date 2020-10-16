b184026997d475311bc970a971cb375a06e37370
jordyjoestar <66799726+jordyjoestar@users.noreply.github.com>
2020-10-16 20:24:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("blmprov");
+$(document).ready(function(){   
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    }); 
+}); 
