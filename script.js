2acb090bccb9924f174ae706af7c64dd791ae3ef
jordyjoestar <66799726+jordyjoestar@users.noreply.github.com>
2020-10-17 11:37:11 +0100

    Update script.js
    added the shit!

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("blmprov");
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/6);
+    yp += ((mouseY - yp)/6);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
